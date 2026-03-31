33a06550a51d7fb5239c4d7b15b677e48dc7ff7d
NorthStar <northrose054@gmail.com>
2026-04-01 00:27:01 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,19 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# رابط قاعدة البيانات
+# تأكدي أن الرابط يبدأ بـ postgresql:// وليس postgres://
 DATABASE_URL = "postgresql://foursa_db_user:Cup5I8073i4K5qpHCWj7tr87WWmmalyb@dpg-d763ge450q8c73ft21m0-a/foursa_db"
 
-# إنشاء المحرك (Engine)
+# في الإصدارات الجديدة وقواعد PostgreSQL لا نحتاج connect_args
 engine = create_engine(DATABASE_URL)
 
-# إنشاء جلسة الاتصال (Session)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# الكلاس الأساسي للجداول
 Base = declarative_base()
 
-# دالة الحصول على قاعدة البيانات
 def get_db():
     db = SessionLocal()
     try:
