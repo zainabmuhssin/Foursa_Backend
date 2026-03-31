9740e2268c9f85b07d35b443ce13590da22d7577
NorthStar <northrose054@gmail.com>
2026-04-01 00:14:59 +0300

    Update database.py

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # رابط قاعدة البيانات
-DATABASE_URL = "sqlite:///./jobs_pro.db"
+DATABASE_URL = "postgresql://foursa_db_user:Cup5I8073i4K5qpHCWj7tr87WWmmalyb@dpg-d763ge450q8c73ft21m0-a/foursa_db"
 
 # إنشاء المحرك (Engine)
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 # إنشاء جلسة الاتصال (Session)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -14,8 +14,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # الكلاس الأساسي للجداول
 Base = declarative_base()
 
-
-# دالة الحصول على قاعدة البيانات (Dependency)
+# دالة الحصول على قاعدة البيانات
 def get_db():
     db = SessionLocal()
     try:
