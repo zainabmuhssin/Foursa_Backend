2a580aee5c725b696625519a37ea180c4a2982f0
zainabmuhssin <northrose054@gmail.com>
2026-04-03 23:00:36 +0300

    clean merge conflicts

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -23,24 +22,3 @@ def get_db():
         yield db
     finally:
         db.close()
-=======
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-# تأكدي أن الرابط يبدأ بـ postgresql:// وليس postgres://
-DATABASE_URL = "postgresql://foursa_db_user:Cup5I8073i4K5qpHCWj7tr87WWmmalyb@dpg-d763ge450q8c73ft21m0-a/foursa_db"
-
-# في الإصدارات الجديدة وقواعد PostgreSQL لا نحتاج connect_args
-engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
->>>>>>> 8ade551520ec340b2fc0393c6483a71d39d3a2cc
